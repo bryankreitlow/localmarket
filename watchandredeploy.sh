f04a8649142bd3c64fcc82a24f2e8e80e4d6e11c
Bryan Kreitlow <bryan@sococo.net>
2013-02-22 19:29:40 -0800

    a process to watch the git repo for changes and restart the node process with forever

---
+++ watchandredeploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# what repository do we want to watch.
+repository="origin/master"
+serverFile="app.js"
+latest_revision="none"
+
+# loop forever, need to kill the process.
+while [ 1 ]; do
+
+    # get the latest revision SHA.
+    current_revision=`git rev-parse $repository`
+
+    # if we haven't seen that one yet, then we know there's new stuff.
+    if [ $latest_revision != $current_revision ]; then
+
+        # mark the newest revision as seen.
+        latest_revision=$current_revision
+
+        # restart the node forever.
+        `sudo forever restart $serverFile`
+
+    fi
+    sleep 60
+done
