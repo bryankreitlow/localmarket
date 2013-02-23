2b1b074c412758c054e3a55afbcfb27c21c0e4c6
Bryan Kreitlow <bryan@sococo.net>
2013-02-22 21:31:26 -0800

    remove whitespace

--- watchandredeploy.sh
+++ watchandredeploy.sh
@@ -7,12 +7,10 @@ LATEST_REVISION="none"
 
 # loop forever, need to kill the process.
 while [ 1 ]; do
-
     # get the latest revision SHA.
     CURRENT_REVISION=$(git ls-remote git://github.com/bryankreitlow/localmarket.git HEAD | cut -c 1-40)
     # if we haven't seen that one yet, then we know there's new stuff.
     if [ $LATEST_REVISION != $CURRENT_REVISION ]; then
-
         # mark the newest revision as seen.
         LATEST_REVISION=$CURRENT_REVISION
 
@@ -23,9 +21,6 @@ while [ 1 ]; do
         # restart the forever process
         echo "Restarting forever process"
         sudo forever restart app.js
-
     fi
-
     sleep 60
-
 done
