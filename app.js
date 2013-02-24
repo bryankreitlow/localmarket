fe37ed4c56af50277e9ffe3391aec5b16645291e
Nathan Scovil <nathan_scovil@yahoo.com>
2013-02-23 16:47:04 -0800

    get more map stuff pushed

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.configure(function() {
   app.set('view engine', 'ejs');
   app.set("view options", { layout: "layout.ejs" });
   app.engine('ejs', engine);
+  app.set('message', 'Hello World')
   app.use(express.logger('dev'));
   app.use(express.cookieParser());
   app.use(express.bodyParser());
@@ -20,8 +21,14 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/map', function(req, res){
+    res.render('map', {
+    message: 'Hello World'
+  });
+});
+
 app.io.set('log level', 1);
 
-app.listen(80);
+app.listen(80, "127.0.0.1");
 
-console.log('Server running at http://www.localmart.com/');
+console.log('Server running at http://www.localmart.me/');
